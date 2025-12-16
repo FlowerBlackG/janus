c16c4cf5c908ce26b8a351bf3484c94cca23ea23
FlowerBlackG <fb2466375479@outlook.com>
2025-12-16 21:29:14 +0800

    OrgJsonExtension.kt: org.json helpers.

---
+++ src/main/kotlin/janus/utils/OrgJsonExtension.kt
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MulanPSL-2.0
+/* From SUFE GuoTuanTuan Project. */
+
+package io.github.flowerblackg.janus.utils
+
+import org.json.JSONObject
+
+/*
+ * Some extensions to org.json library.
+ */
+
+/**
+ * get string, and returns null instead of throws.
+ */
+fun JSONObject.getStringOrNull(key: String) = this.getString(key, null)
+
+/**
+ * get string, and returns default value instead of throws.
+ */
+fun JSONObject.getString(key: String, defaultValue: String?) = try {
+    this.getString(key)
+} catch (_: Exception) {
+    defaultValue
+}
+
+
+fun JSONObject.getString(key: String, defaultValue: String) = this.getString(key, defaultValue as String?)!!
+
+
+fun JSONObject.getInt(key: String, defaultValue: Int?) = try {
+    this.getInt(key)
+} catch (_: Exception) {
+    defaultValue
+}
+
+fun JSONObject.getInt(key: String, defaultValue: Int) = this.getInt(key, defaultValue as Int?)!!
+
+fun JSONObject.getBoolean(key: String, defaultValue: Boolean?) = try {
+    this.getBoolean(key)
+} catch (_: Exception) {
+    defaultValue
+}
+
+fun JSONObject.getBoolean(key: String, defaultValue: Boolean) = this.getBoolean(key, defaultValue as Boolean?)!!

17c10044b8d2508b6b323439e09aeed655c1d66f
FlowerBlackG <fb2466375479@outlook.com>
2026-01-01 17:30:39 +0800

    add global netty event loop group, and use it as kotlin coroutines' dispatcher.

--- src/main/kotlin/janus/coroutine/GlobalCoroutineScopes.kt
+++ src/main/kotlin/janus/coroutine/GlobalCoroutineScopes.kt
@@ -2,10 +2,18 @@
 
 package io.github.flowerblackg.janus.coroutine
 
+import io.netty.channel.MultiThreadIoEventLoopGroup
+import io.netty.channel.nio.NioIoHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
 
 object GlobalCoroutineScopes {
-    val IO: CoroutineScope = CoroutineScope(Dispatchers.IO)
-    val Default: CoroutineScope = CoroutineScope(Dispatchers.Default)
+    private val nCpus = Runtime.getRuntime().availableProcessors()
+    private val nThreads = maxOf(4, nCpus * 2)
+    val nettyEventLoopGroup = MultiThreadIoEventLoopGroup(nThreads, NioIoHandler.newFactory())
+
+    val nettyCoroutineDispatcher = nettyEventLoopGroup.asCoroutineDispatcher()
+
+    val IO: CoroutineScope = CoroutineScope(nettyCoroutineDispatcher)
+    val Default: CoroutineScope = CoroutineScope(nettyCoroutineDispatcher)
 }
